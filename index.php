47547007979911ad844b9c76cfb14c61b93cdd67
Carine0509 <carine.gomes.macedo@gmail.com>
2022-06-07 17:14:45 -0300

    First commit. Sample PHP composer and lib Slugify

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use Cocur\Slugify\Slugify;
+
+$slugify = new Slugify();
+echo $slugify->slugify('Hello World!'); // hello-world
+
+echo "</br>";
+
+echo $slugify->slugify('Hello World!', '_'); // hello_world
+
+?>
